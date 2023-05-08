83c6035cf9a3307278532ad2813d25ac397545b3
dienoov <alvinodien@yahoo.co.id>
2023-05-08 16:03:43 +0700

    Fix NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,7 @@
 <template>
   <header class="flex w-full md:h-32">
     <h1 class="w-1/2">
-      <router-link to="/" class="md:text-xl text-lg font-bold tracking-tight block
+      <router-link to="/" class="md:text-xl text-lg font-bold tracking-tighter block
           md:p-4 p-2 h-full border-b-2 border-black">
         PuddingFest
       </router-link>
@@ -13,15 +13,15 @@
     <nav class="w-1/2">
       <ul class="flex h-full">
         <li class="w-1/2">
-          <router-link to="/" class="md:text-xl text-lg font-bold tracking-tight block
+          <router-link to="/event" class="md:text-xl text-lg font-bold tracking-tighter block
               md:p-4 p-2 h-full border-b-2 border-l-2 border-black">
-            Home
+            Event
           </router-link>
         </li>
         <li class="w-1/2">
-          <router-link to="/" class="md:text-xl text-lg font-bold tracking-tight block
+          <router-link to="/contact" class="md:text-xl text-lg font-bold tracking-tighter block
               md:p-4 p-2 h-full border-b-2 border-l-2 border-black">
-            About
+            Contact
           </router-link>
         </li>
       </ul>
