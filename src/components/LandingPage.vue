fb71408b11c48850cc94b43e5897792d4a2ac816
dienoov <alvinodien@yahoo.co.id>
2023-05-08 15:28:33 +0700

    Add content to landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-  <p>Hello</p>
+  <main class="xl:max-w-[90rem] max-w-[90%] mx-auto">
+    <h2 class="xl:text-[16rem] lg:text-[13rem] md:text-[9rem] text-[5rem]
+        font-bold tracking-tighter">
+      Innovate
+    </h2>
+    <h3 class="xl:text-7xl lg:text-5xl md:text-4xl text-2xl
+        uppercase font-medium tracking-tighter xl:max-w-[60rem] md:max-w-[60%]">
+      Discover the world of tech with us.
+    </h3>
+  </main>
 </template>
 
 <style scoped>
