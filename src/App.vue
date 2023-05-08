93f476a8c8633857ecf3252f1ab0b15101131b48
dienoov <alvinodien@yahoo.co.id>
2023-05-08 14:00:23 +0700

    Add NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-
+  <NavBar/>
+  <router-view/>
 </template>
 
 <style scoped>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="flex w-full md:h-32">
+    <h1 class="w-1/2">
+      <router-link to="/" class="md:text-xl text-lg font-bold tracking-tight block
+          md:p-4 p-2 h-full border-b-2 border-black">
+        PuddingFest
+      </router-link>
+    </h1>
+    <nav class="w-1/2">
+      <ul class="flex h-full">
+        <li class="w-1/2">
+          <router-link to="/" class="md:text-xl text-lg font-bold tracking-tight block
+              md:p-4 p-2 h-full border-b-2 border-l-2 border-black">
+            Home
+          </router-link>
+        </li>
+        <li class="w-1/2">
+          <router-link to="/" class="md:text-xl text-lg font-bold tracking-tight block
+              md:p-4 p-2 h-full border-b-2 border-l-2 border-black">
+            About
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
