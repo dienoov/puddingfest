995211c19da7c934e0fa1fbab715a95f6bc90fc8
dienoov <alvinodien@yahoo.co.id>
2023-05-08 14:06:00 +0700

    Create router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue';
 import './style.css';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import LandingPage from './components/LandingPage.vue';
 
-createApp(App).mount('#app');
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{
+    path: '/',
+    component: LandingPage,
+  }],
+});
+
+createApp(App)
+  .use(router)
+  .mount('#app');
