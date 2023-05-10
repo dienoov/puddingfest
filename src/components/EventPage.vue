3c4d503386b7f5be91802280184d21d221728c34
dienoov <alvinodien@yahoo.co.id>
2023-05-10 21:51:18 +0700

    Add landing page description

--- src/components/EventPage.vue
+++ src/components/EventPage.vue
@@ -5,6 +5,20 @@ import MainHeader from './MainHeader.vue';
 <template>
   <main>
     <MainHeader title="Events" subtitle="Experience the Cutting Edge of Tech." />
+    <article class="xl:max-w-[90rem] max-w-[90%] mx-auto lg:my-20 my-12">
+      <p class="tracking-tight my-10 font-medium lg:text-3xl text-xl">
+        The premier event for technology enthusiasts, entrepreneurs, and innovators from around
+        the world! Our mission is to inspire, connect, and empower the tech community by providing
+        an immersive experience that showcases the latest trends, fosters collaboration, and
+        facilitates meaningful networking.
+      </p>
+      <p class="tracking-tight my-10 font-medium lg:text-3xl text-xl">
+        You'll have the opportunity to engage with industry leaders, discover emerging technologies,
+        and gain insights from experts in your field. Whether you're a startup founder, an
+        established business executive, a developer, or a designer, there's something for everyone
+        at our event.
+      </p>
+    </article>
   </main>
 </template>
 
