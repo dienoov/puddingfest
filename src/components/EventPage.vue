3de471cf728def861ebcd865a375dbcfac8caab6
dienoov <alvinodien@yahoo.co.id>
2023-05-10 23:46:41 +0700

    Fix carousel

--- src/components/EventPage.vue
+++ src/components/EventPage.vue
@@ -1,37 +1,37 @@
 <script setup>
-// eslint-disable-next-line no-unused-vars
-import { Carousel } from 'flowbite';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Carousel } from 'flowbite-vue';
+import { onMounted } from 'vue';
 import MainHeader from './MainHeader.vue';
 
-import carousel1 from '../assets/carousel-1.jpg';
-import carousel2 from '../assets/carousel-2.jpg';
-import carousel3 from '../assets/carousel-3.jpg';
-
-const carousels = [{
-  id: 0,
-  image: carousel1,
+const pictures = [{
+  src: '/carousel-1.jpg',
+  alt: 'img',
 }, {
-  id: 1,
-  image: carousel2,
+  src: '/carousel-2.jpg',
+  alt: 'img',
 }, {
-  id: 2,
-  image: carousel3,
+  src: '/carousel-3.jpg',
+  alt: 'img',
 }];
+
+onMounted(() => {
+  const carouselEl = document.querySelector('#default-carousel div');
+  carouselEl.classList.remove('rounded-lg', 'h-56', 'sm:h-64', 'xl:h-80', '2xl:h-96');
+  carouselEl.classList.add('h-[80vh]', 'max-h-[42rem]');
+
+  const imgEls = document.querySelectorAll('#default-carousel img');
+  imgEls.forEach((imgEl) => {
+    imgEl.classList.add('object-cover', 'h-full');
+  });
+});
 </script>
 
 <template>
   <main>
     <MainHeader title="Events" subtitle="Experience the Cutting Edge of Tech."/>
     <article class="xl:max-w-[90rem] max-w-[90%] mx-auto my-12">
-      <div class="relative w-full" data-carousel="slide">
-        <div class="relative h-[80vh] max-h-[42rem] overflow-hidden">
-          <div class="hidden duration-1000 ease-in-out" v-for="carousel in carousels"
-               :key="carousel.id" :data-carousel-item="carousel.id === 0 ? 'active' : ''">
-            <img class="absolute block w-full -translate-x-1/2 -translate-y-1/2
-              top-1/2 left-1/2 object-cover h-full" alt="..." :src="carousel.image">
-          </div>
-        </div>
-      </div>
+      <Carousel :pictures="pictures" :controls="false" :indicators="false" :slide="true"></Carousel>
     </article>
   </main>
 </template>
